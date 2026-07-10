1ad5defdffd207bc31b2c01578552dd57dcf86fc
Albaaaaa <albasierod@gmail.com>
2026-07-10 19:09:48 +0200

    add get/api tareas

--- 20260708-api-tareas/index.js
+++ 20260708-api-tareas/index.js
@@ -1,68 +1,71 @@
 //PRIMER SERVIDOR WEB CON EXPRESS
-//1.Importaamos la librería express
+//1. Importamos la librería Express.
 //Express nos permite crear servidores web de forma sencilla
 const express = require("express");
-//2.Creamos una aplicacion con Express
-//La variable "app" sera nuestro servidor
-const app = express ();
-//3.Definimos el puerto donde escuchará el servidor
-//En este caso utilizamos el puerto 3000
+//2. Creamos una aplicación con Express.
+//La variable "app" será nuestro servidor
+const app = express();
+//3. Definmos el puerto donde escuchará el servidor
+// En esta caso utilizaremos el puerto 3000
 const PORT = 3000;
-//4.Iniciamos el servidor
-//listen()hace que el servidor quede esperando aplicaciones
-//de los clientes(por ejemplo,desde un navegador)
+//4. Iniciamos el servidor
+//listen() hace que el servidor quede esperando peticiones
+//de los clientes (por ejemplo, desde un navegador)
 
-//==========================================================
-//MIDDLEAWARE
-//==========================================================
-//Un middleware es una funcion que se ejecuta anteds de
+//================================================
+//MIDDLEWARE
+//================================================
+// Un middleware es una función que se ejecuta antes de
 //llegar a las rutas.
-//express.json()convierte automáticamente los datos
-//enviados en formato JSON en un objeto JavaScript
-//Gracias a este middleware podremos acceder a:
+// express.json()convierte automáticamente los datos
+//enviados en formato JSON en un objeto JavaScript.
+//Gracias a este middleware podremos acceder a :
 // req.body
-//cuando el cliente envíe informacion mediante POST o PUT.
+//cuando el cliente envíe información mediate POST o PUT.
 app.use(express.json());
 
-//==========================================================
-//BASE
-//==========================================================
-//Simulamos una base de datos utilizando un arreglo
+//=========================================
+//BASE DE DATOS EN MEMORIA
+//=========================================
+// Simulamos una base de datos utilizando un arreglo
 //IMPORTANTE:
 //Los datos solo existen mientras el servidor está
 //encendido
-//Si detenemos Node.js, toda esta informacion se pierde
+//Si detenemos Node.js, toda esta información se pierde
 let tareas = [
-    //Primera tarea
+    // Primera tarea
     {
         id: 1,
-        título:"Aprender Express",
+        titulo: "Aprender Express",
         completada: false
     },
-    //Segunda tarea
+    // Segunda tarea
     {
         id:2,
-        título:"Estudiar Node.js",
-        completada:true
+        titulo: "Estudiar Node.js",
+        completada: true
     },
-    //Tercera tarea
+    // Tercera tarea
     {
         id:3,
-        título:"practicar Thunder client",
+        titulo: "Practicar Thunder Client",
         completada:false
     }
 
 ];
 
-//========================================================================
+//================================
 //RUTA PRINCIPAL
-//========================================================================
-app.get("/"(req, rest)=> {
-    res.send("🚀 Bienvenido a la API REST de Tareas
+//================================
+app.get("/", (req, res)=> {
+    res.send("🚀 Bienvenido a la API REST de Tareas");
 })
 
-app.listen( PORT , ()=>{
-    //5.Cuando el servidor se inicia correctamente,
+
+app.listen(PORT, () => {
+    //5. Cuando el servidor se incicia correctamente,
     //mostramos un mensaje en la consola.
-    console.log(`💕Servidor ejecutándose en http://localhost:${PORT}`);
+    console.log(`🎉Servidor ejecutándose en http://localhost:${PORT}`);
 });
+
+
